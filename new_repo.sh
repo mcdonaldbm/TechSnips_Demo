252bed44665e02b9aa23520b1b21210e92484c4a
First User <user01@brycematthew.net>
2018-06-12 10:29:19 -0700

    Initial Commit

---
+++ new_repo.sh
@@ -0,0 +1,14 @@
+# Initialize a new Repo
+git init
+
+# Set up remote repo
+git remote add origin 
+
+# Add files to first commit
+git add .
+
+# Make first commit
+git commit -m "Initial Commit"
+
+# Push first commit
+git push origin master
